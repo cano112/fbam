6275ef28e539c75629f5eac47c6db4b1f7e091e4
Kamil Noster <kamil.noster@gmail.com>
2020-04-28 02:08:26 +0200

    Update file path. Add version file

--- common/commons.h
+++ common/commons.h
@@ -3,7 +3,7 @@
 
 #include <unistd.h>
 
-#define LOG_FILE_PATH "/tmp/file_access.log"
+#define LOG_FILE_PATH "/fbam/file_access.log"
 #define MAX_LOG_LEN (MAXPATHLEN + 50)
 
 #endif //FBAM_COMMONS_H
